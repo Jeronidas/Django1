4576d804f734761baaf67df99d2da857ecd6d9af
jeremias <jerechercato@gmail.com>
2024-10-15 18:25:14 -0300

    'vamos adelante'

---
+++ inicio/forms.py
@@ -0,0 +1,10 @@
+from django import forms 
+
+class CrearAutoFormulario(forms.Form):
+    marca = forms.CharField(max_length=20)
+    modelo = forms.CharField(max_length=20)
+    anio = forms.IntegerField()
+    
+
+class BuscarAutoFormulario(forms.Form):
+    marca = forms.CharField(max_length=20, required=False)
